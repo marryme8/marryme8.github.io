1afe93765d8b00985c9fcc80c04d6d134b08589c
yufoo1 <yufoo1.cs@gmail.com>
2024-12-09 00:30:58 +0800

    change intro

---
+++ src/pages/intro-second/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+import './index.css'
+
+export default function Hello() {
+    return (
+      <Layout title="Hello" description="Hello React Page">
+        <div className='intro-div'>
+          <h1 className='intro-h1-text'>团队简介</h1>
+          <div style={{width: "50%", textIndent: 32}}>
+          我们的团队由三位充满热情和研究志向的成员组成，专注于婚俗的研究，力求从历史、文化及社会的多维度探讨婚礼习俗的演变与传承。作为跨学科的研究团队，我们的成员分别在语言学、社会学、人类学等不同领域积累了深厚的理论功底与实践经验，形成了对婚俗这一传统文化现象的综合性理解。我们相信，通过对婚俗的深入研究，能够为中国传统文化的保护与创新、以及现代婚礼文化的反思与发展，提供有益的学术支持和文化启示。
+          </div>
+          <p></p>
+          <h1 className='intro-h1-text'>研究的方向与意义</h1>
+          <div style={{width: "50%", textIndent: 32}}>
+            <div>我们团队的研究方向集中在婚俗文化的传承与变迁，尤其是从语言学、社会学、文化人类学等多个角度，探讨婚俗习惯的形成、演变及其文化内涵。具体而言，团队的研究内容包括：</div>
+            <p></p>
+            <div>1. 婚俗的语言学分析</div>
+            <div>婚俗往往通过言语和符号传递文化信息。从婚礼用语、民俗歌谣到婚礼的口头表达，语言承载着丰富的社会意义和文化象征。我们团队成员在语言学方面的研究，主要聚焦于婚俗中使用的特定词汇、成语、祝福语及其背后的文化解读。通过对这些婚俗语言的研究，我们旨在揭示其在不同历史时期的变化及其背后的文化动力，进一步分析婚俗语言与社会心理、价值观念之间的关系。</div>
+            <p></p>
+            <div>2. 婚俗的社会学与人类学研究</div>
+            <div>婚俗作为一种深具社会性和文化性的行为，其背后反映着家庭结构、性别关系、社会地位及权力机制。通过对婚俗仪式的观察和分析，我们可以揭示出一个社会对婚姻、性别、家庭的态度。我们团队在社会学和人类学方面的研究，关注婚俗与社会阶层、宗族结构、地域差异等方面的联系，尤其是在不同历史背景下婚俗如何受到社会变革和制度演化的影响。例如，传统婚俗中的礼金、媒人、婚姻安排等，反映了历史上父权社会、等级制度等社会因素。</div>
+          </div>
+          <p></p>
+          <h1 className='intro-h1-text'>团队成员介绍</h1>
+          <div style={{width: "50%", textIndent: 32}}>
+          <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+            <img src="http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg" width='20%'></img>
+          </div>
+          <p></p>
+          <div>奶龙，学术背景深厚，研究兴趣广泛，尤其专注于婚俗文化与传统习俗的演变与传承。作为一名学者，奶龙致力于从语言学、社会学及人类学等多个学科角度，深入探讨传统婚俗的文化内涵、历史背景以及现代社会中的变迁。通过跨学科的综合研究方法，奶龙在婚俗领域的研究已涵盖了婚礼习俗的语言特征、社会结构、文化传承等方面，并在相关领域中发表了多篇学术论文。</div>
+          </div>
+          <div style={{width: "50%", textIndent: 32}}>
+          <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+            <img src="http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg" width='20%'></img>
+          </div>
+          <p></p>
+          <div>奶龙，学术背景深厚，研究兴趣广泛，尤其专注于婚俗文化与传统习俗的演变与传承。作为一名学者，奶龙致力于从语言学、社会学及人类学等多个学科角度，深入探讨传统婚俗的文化内涵、历史背景以及现代社会中的变迁。通过跨学科的综合研究方法，奶龙在婚俗领域的研究已涵盖了婚礼习俗的语言特征、社会结构、文化传承等方面，并在相关领域中发表了多篇学术论文。</div>
+          </div>
+          <div style={{width: "50%", textIndent: 32}}>
+          <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}>
+            <img src="http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg" width='20%'></img>
+          </div>
+          <p></p>
+          <div>奶龙，学术背景深厚，研究兴趣广泛，尤其专注于婚俗文化与传统习俗的演变与传承。作为一名学者，奶龙致力于从语言学、社会学及人类学等多个学科角度，深入探讨传统婚俗的文化内涵、历史背景以及现代社会中的变迁。通过跨学科的综合研究方法，奶龙在婚俗领域的研究已涵盖了婚礼习俗的语言特征、社会结构、文化传承等方面，并在相关领域中发表了多篇学术论文。</div>
+          </div>
+          <p></p>
+        </div>
+      </Layout>
+    );
+  }
